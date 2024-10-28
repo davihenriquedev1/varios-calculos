e6b0f9abdcab4cda1bdba1e76ca15f69d80d9914
davihenriquelima <davihenrique.ads@gmail.com>
2024-10-28 17:59:01 -0300

    chore: add the base features from the shadcn-UI library
    on branch main
    
    changes to be submitted:
    	modified:   .gitignore
    	new file:   .hintrc
    	new file:   components.json
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   src/app/globals.css
    	modified:   src/app/layout.tsx
    	modified:   src/app/page.tsx
    	new file:   src/components/mode-toggle.tsx
    	new file:   src/components/theme-provider.tsx
    	new file:   src/components/ui/button.tsx
    	new file:   src/components/ui/dropdown-menu.tsx
    	new file:   src/lib/utils.ts
    	modified:   tailwind.config.ts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
-export default function Home() {
+const Home = () => {
 	return (
-		<div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-		
+		<div className="flex flex-col w-full items-center justify-center min-h-screen">
+			<h1 className="text-3xl font-bold">Em breve</h1>
+			<p>Página em desenvolvimento</p>
 		</div>
 	);
 }
+
+export default Home;
