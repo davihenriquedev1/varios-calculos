455974e9c104bea261f2c2daca326f1f43f74b56
davihenriquelima <davihenrique.ads@gmail.com>
2024-11-14 18:38:10 -0300

    refactor: add 3nd section on home page
    on branch develop
    
    changes to be committed:
        new file:   public/images/calc-icon.png
        new file:   "public/images/gr\303\241ficos.png"
        new file:   public/images/pdf.png
        new file:   public/images/video-tutorial.png
        new file:   src/app/(calculators)/bmi/page.tsx
        renamed:    src/app/(calculators)/layout.tsx -> src/app/(calculators)/currency-conversion/page.tsx
        new file:   src/app/(calculators)/investments/page.tsx
        new file:   src/app/(calculators)/loans/page.tsx
        new file:   src/app/(calculators)/personal-finance/page.tsx
        new file:   src/app/(calculators)/scientific/page.tsx
        modified:   src/app/page.tsx
        modified:   src/components/home/Frame.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,25 +17,45 @@ const Home = () => {
 				</div>
 			</section>
 
-			<section className="flex flex-col w-full px-4 py-10 gap-8 md:px-10 md:py-14 md:h-screen bg-gradient-to-t from-color-palette1/5 to-gray-500/5">
+			<section className="flex flex-col w-full px-4 py-10 gap-8 md:px-10 md:py-14 lg:h-screen bg-gradient-to-t from-color-palette1/5 to-gray-500/5">
 				
-				<div className="flex w-full justify-center items-center ">
-					<h2 className="font-extrabold tracking-wide text-color-palette4 text-center text-2xl md:text-5xl">Múltiplas Calculadoras</h2>
+				<div className="flex w-full md:justify-center md:items-center ">
+					<h2 className="font-extrabold tracking-wide md:text-center text-4xl md:text-6xl text-color-palette2">Múltiplas Calculadoras</h2>
 				</div>
 				<div className="grid grid-cols-2 h-full gap-3 md:grid-cols-3 md:gap-8">
-					<Frame title="Calculadora de IMC" desc="Calcule seu Índice de Massa Corporal."/>
-					<Frame title="Calculadora Cíentífica" desc="Use nossa calculadora avançada."/>
-					<Frame title="Calculadora de Investimentos" desc="Calcule seus rendimentos."/>
-					<Frame title="Calculadora de Empréstimos" desc="Calcule o quanto você vai pagar."/>
-					<Frame title="Calculadora de Conversão de Moedas" desc="Veja a conversão de Real pra Dólar e muito mais."/>
-					<Frame title="Calculadora de Finanças Pessoais" desc="Planeje suas finanças pessoais."/>
+					<Frame route="/bmi" title="Calculadora de IMC" desc="Calcule seu Índice de Massa Corporal."/>
+					<Frame route="/scientific" title="Calculadora Cíentífica" desc="Use nossa calculadora avançada."/>
+					<Frame route="/investments" title="Calculadora de Investimentos" desc="Calcule seus rendimentos."/>
+					<Frame route="/loans" title="Calculadora de Empréstimos" desc="Calcule o quanto você vai pagar."/>
+					<Frame route="/currency-conversion" title="Calculadora de Conversão de Moedas" desc="Veja a conversão de Real pra Dólar e muito mais."/>
+					<Frame route="/personal-finance" title="Calculadora de Finanças Pessoais" desc="Planeje suas finanças pessoais."/>
 				</div>
 
+			</section >
+
+			<section className="flex flex-col w-full px-4 py-10 gap-8 md:px-10 md:py-14 md:h-screen bg-gradient-to-b from-color-palette1/5 to-color-palette1/50">
+				<div className="flex md:justify-center md:items-center">
+					<h2 className="font-extrabold tracking-wide md:text-center text-4xl md:text-6xl text-color-palette4">Funcionalidades Extras</h2>
+				</div>
+				<div className="flex flex-col md:flex-row flex-1 gap-8 md:gap-10 ">
+					<div className="flex flex-col items-center justify-end flex-1 gap-3 ">
+						<img src="/images/video-tutorial.png" alt="tutorial-icon" className="h-52 w-auto rounded-lg shadow-xl bg-gradient-to-r from-slate-50/30 to-slate-50/5"/>
+						<h3 className="text-xl font-semibold text-center text-color-palette4">Tutorial em Vídeo</h3>
+						<p className="text-center text-sm text-foreground mt-2">Assista a um tutorial explicativo sobre como usar as funcionalidades do site.</p>
+					</div>
+					<div className="flex flex-col items-center justify-center flex-1 gap-3">
+						<img src="/images/gráficos.png" alt="grafics-icon" className="h-52 w-auto rounded-lg shadow-xl bg-gradient-to-r from-slate-50/30 to-slate-50/5"/>
+						<h3 className="text-xl font-semibold text-center text-color-palette4">Gráficos Interativos</h3>
+						<p className="text-center text-sm text-foreground mt-2">Visualize os dados de forma clara e precisa através de gráficos interativos.</p>
+					</div>
+					<div className="flex flex-col items-center justify-start flex-1 gap-3">
+						<img src="/images/pdf.png" alt="pdf download icon" className="h-52 w-auto rounded-lg shadow-xl bg-gradient-to-r from-slate-50/30 to-slate-50/5"/>
+						<h3 className="text-xl font-semibold text-center text-color-palette4">Download em PDF</h3>
+						<p className="text-center text-sm text-foreground mt-2">Baixe relatórios completos em formato PDF para análise offline.</p>
+					</div>
+				</div>
 			</section>
 
-			<section>
-
-			</section>
 		</main>
 	);
 }
--- src/components/home/Frame.tsx
+++ src/components/home/Frame.tsx
@@ -1,18 +1,24 @@
+import Link from "next/link"
+import { Button } from "../ui/button"
+
 type Props = {
+    route:string,
     title?:string,
     desc?:string,
     bg?:string
 }
 
-export const Frame = ({title, desc, bg}:Props) => {
+export const Frame = ({route, title, desc, bg}:Props) => {
 
     return (
-        <div className={`flex flex-1 flex-col p-4 gap-3 justify-between shadow-lg shadow-color-palette1 rounded-lg hover:scale-105 transition-all duration-500 ${bg ? 'bg-cover bg-center':'bg-gradient-to-r from-slate-50/40 to-slate-200/40'} md:flex-row`} style={bg ? {background: `url(${bg})`} : {}}>
+        <div className={`flex flex-1 flex-col p-4 gap-3 justify-between shadow-lg shadow-color-palette1 rounded-lg hover:scale-105 transition-all ease-in-out duration-700 ${bg ? 'bg-cover bg-center':'bg-gradient-to-r from-slate-50/40 to-slate-200/40'} lg:flex-row`} style={bg ? {background: `url(${bg})`} : {}}>
             <div className="flex flex-col">
                 <h2 className="text-lg font-semibold mb-2">{title}</h2>
                 <p className="text-sm mb-4 text-secondary-foreground">{desc}</p>
             </div>
-            <button type="button" className="bg-color-palette1 text-white px-4 py-2 rounded font-bold hover:brightness-105 transition-all">Acessar</button>
+            <Button type="button" className="bg-color-palette2 text-white px-4 py-2 rounded font-bold hover:brightness-105 transition-all">
+                <Link href={route?route:'/'}>Acessar</Link>
+            </Button>
 		</div>
     )
 }
